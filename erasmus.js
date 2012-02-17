641a181daa573e84580d4f5ee4c29882f892bd02
Miguel Regala <mikfisher64@gmail.com>
2012-02-17 20:37:10 +0000

    Query limit solved

--- erasmus.js
+++ erasmus.js
@@ -2,7 +2,9 @@ var jsonPath = "students.php"
 var map;
 var geocoder;
 var queryTries = 50;
-var queryTimeout = 100;
+var queryTimeout = 200;
+var totalMarkers = 0;
+var numberStudents = 0;
 
 
 function sleep(milliSeconds){
@@ -28,57 +30,65 @@ function loadPoints() {
         data: '',
         success: function (data) {
             console.log('Data extracted:' + data);
+			console.log('\n ' + data.length + ' students ');
+			numberStudents = data.length;
 
 			$.each(data, function(index, student) {
-				console.log('address: ' + student[0] + ' , ' + student[1]);
-				setTimeout( addMarker(student), queryTimeout * index);
+				var timeout = queryTimeout * index;
+				setTimeout( function(){addMarker(student, timeout);}, timeout);
 			});
         },
         error: function (data) {
             console.log('Error loading markers' + data);
         }
     });
-	console.log('finish');
 }
 
-function addMarker( student ) {
+function addMarker( student , timeout) {
 
 	geocoder.geocode( { 'address': student[3] }, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
-				var delta = Math.pow(10, -3);
-				var lat = results[0].geometry.location.lat() + (Math.random() * (2 * delta)) - delta;
-				var lng = results[0].geometry.location.lng() + (Math.random() * (2 * delta)) - delta;
-				var randPosition = new google.maps.LatLng(lat, lng);
-				
-				var marker = new google.maps.Marker({
-				map: map,
-				position: randPosition,
-				cursor: student[0] + ', ' + student[1], 
-				title: student[0] + ', ' + student[1],
-				animation: google.maps.DROP
-			});
+			var delta = Math.pow(10, -3);
+			var lat = results[0].geometry.location.lat() + (Math.random() * (2 * delta)) - delta;
+			var lng = results[0].geometry.location.lng() + (Math.random() * (2 * delta)) - delta;
+			var randPosition = new google.maps.LatLng(lat, lng);
 			
-				var contentString = '<div id="content">'+
-				'<div id="siteNotice">'+
-				'</div>'+
-				'<h2 id="firstHeading" class="firstHeading">' + student[3] + ', ' + student[2] + '</h2>'+
-				'<div id="bodyContent">'+
-				'<p>' + results[0].address_components[0].long_name +  '</p>' +
-				'</div>'+
-				'</div>';
-				var infowindow = new google.maps.InfoWindow({ content: contentString });
-				
-			 google.maps.event.addListener(marker, "click", function() {
-				map.panTo(results[0].geometry.location);
-				infowindow.open(map,marker);
-			});
+			//console.log('timeout: ' + queryTimeout * index);
+			console.log('Adding ' + student[0] + ' ... (' + totalMarkers + '/' + numberStudents + ')');
 			
-			/*
-			google.maps.event.addListener(marker, "mouseout", function() {
-				marker.setTitle(student[0] + ', ' + student[1]);
-			});
-			*/
-
+			var marker = new google.maps.Marker({
+			map: map,
+			position: randPosition,
+			cursor: student[0] + ', ' + student[1], 
+			title: student[0] + ', ' + student[1],
+			animation: google.maps.DROP
+		});
+		
+			var contentString = '<div id="content">'+
+			'<div id="siteNotice">'+
+			'</div>'+
+			'<h2 id="firstHeading" class="firstHeading">' + student[3] + ', ' + student[2] + '</h2>'+
+			'<div id="bodyContent">'+
+			'<p>' + results[0].address_components[0].long_name +  '</p>' +
+			'</div>'+
+			'</div>';
+			var infowindow = new google.maps.InfoWindow({ content: contentString });
+			
+		 google.maps.event.addListener(marker, "click", function() {
+			map.panTo(results[0].geometry.location);
+			infowindow.open(map,marker);
+		});
+		
+		/*
+		google.maps.event.addListener(marker, "mouseout", function() {
+			marker.setTitle(student[0] + ', ' + student[1]);
+		});
+		*/
+		
+		totalMarkers++;
 		}
+	else 
+		setTimeout( function(){addMarker(student, timeout);}, timeout);
+		
 	});
 }
