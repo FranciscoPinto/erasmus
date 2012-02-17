6d51042309181a30a4496dff5ca9a0178bb3fd89
Miguel Regala <mikfisher64@gmail.com>
2012-02-17 17:20:37 +0000

    Timeout added

--- erasmus.js
+++ erasmus.js
@@ -1,6 +1,14 @@
-var jsonPath = "http://f.rancis.co/erasmus/students.php"
+var jsonPath = "students.php"
 var map;
 var geocoder;
+var queryTries = 50;
+var queryTimeout = 100;
+
+
+function sleep(milliSeconds){
+	var startTime = new Date().getTime();				     // get the current time
+	while (new Date().getTime() < startTime + milliSeconds); // hog cpu
+}
 
 function initialize() {
     var myOptions = {
@@ -20,22 +28,35 @@ function loadPoints() {
         data: '',
         success: function (data) {
             console.log('Data extracted:' + data);
-			console.log('Data extracted length:' + data.length);
 
 			$.each(data, function(index, student) {
 				console.log('address: ' + student[0] + ' , ' + student[1]);
-				geocoder.geocode( { 'address': student[3] }, function(results, status) {
-			    if (status == google.maps.GeocoderStatus.OK) {
-				map.setCenter(results[0].geometry.location);
-				var marker = new google.maps.Marker({
-					map: map,
-					position: results[0].geometry.location,
-					title: student[0] + ' , ' + student[1]
-				});
-				} else {
-				alert("Geocode was not successful for the following reason: " + status);
+				
+				var error;
+				var timeout = 0;
+				do {
+					error = false;
+					geocoder.geocode( { 'address': student[3] }, function(results, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						map.setCenter(results[0].geometry.location);
+						var marker = new google.maps.Marker({
+							map: map,
+							position: results[0].geometry.location,
+							title: student[0] + ' , ' + student[1]
+					});
+					} else {
+						console.log("Geocode was not successful for the following reason: " + status);
+						error = true; 
+						if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+							timeout++;
+							console.log('timeout: ' + timeout);
+							sleep(queryTimeout);		
+							}
+						}
+					});
+					
 				}
-				});
+				while (error == true && timeout < queryTries);
 			
 			});
         },
